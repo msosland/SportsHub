0fe8d4fe636dae7013a4127b39eac45a396204bd
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-02 10:39:54 -0500

    create second component header strip

--- app/assets/javascripts/components/homepage.es6.jsx
+++ app/assets/javascripts/components/homepage.es6.jsx
@@ -2,6 +2,9 @@ class Homepage extends React.Component {
   render () {
     return (
       <div>
+        <header>
+          <Headerstrip />
+        </header>
         <div>Text: {this.props.text}</div>
       </div>
     );
