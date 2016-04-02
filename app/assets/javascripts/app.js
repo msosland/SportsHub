5f776c55cd123eceddbe3308721f32492564247f
Marshall Sosland <marshall.sosland@gmail.com>
2016-04-02 16:22:48 -0500

    setup home route

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,15 @@
-angular.module('sportsHub', [])
+angular.module('sportsHub', ['ui-router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html'
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }])
